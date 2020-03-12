f30da0d4bdeea62b4c7d7dcb410c145fada35abe
chengguan9616 <chengguan9616@yeah.net>
2020-03-12 18:31:33 +0800

    提交

---
+++ upfile.js
@@ -0,0 +1,28 @@
+let createConnect_func = require("./dbutil");
+// let connection  = createConnect_func.createConnect();
+
+
+function upfile (num,success){
+    let querysql = "insert into uploadlist (`mimetype`,`originalname`,`path`,`filename`,`id`) values(?,?,?,?,?);";
+    let connection = createConnect_func.createConnect();
+    connection.connect();
+    connection.query(querysql,num,function(error,data){
+        if(data){
+            console.log(data);
+            let result = "";
+            for(let i = 0; i < data.length;i++){
+                // console.log(i);
+                result += data[i].name;
+            }
+            console.log(result); 
+            success(data[0]);
+        }else{
+            console.log(error);
+        }
+    })
+    connection.end();
+}
+
+module.exports = {
+    "upfile":upfile
+}
